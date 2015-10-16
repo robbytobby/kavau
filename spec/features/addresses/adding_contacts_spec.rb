2dbb2248e81ab1beb030eeb381390041f3fb6464
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2015-10-16 08:35:37 +0200

    specs for contacts

--- spec/features/addresses/adding_contacts_spec.rb
+++ spec/features/addresses/adding_contacts_spec.rb
@@ -1,11 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe "adding contacts to addresses" do
-  it "is possible to add a contact to a project_address" do
-    @address = create :project_address
-    visit project_address_path(@address)
-    click_on 'add_contact'
-    expect(current_path).to eq(new_contact_path)
+['Organization', 'ProjectAddress'].each do |type|
+  RSpec.describe "adding contacts to #{type.underscore.pluralize}" do
+    before(:each){ @address = create type.underscore.to_sym }
+
+    it "is possible to add a contact to a #{type.underscore}" do
+      visit model_path(@address)
+      click_on 'add_contact'
+      expect(current_path).to eq(send("new_#{type.underscore}_contact_path", @address))
+      fill_in :contact_first_name, with: 'First Name'
+      fill_in :contact_name, with: 'Name'
+      click_on :submit
+      expect(current_path).to eq(model_path(@address))
+      expect(page).to have_selector('div.alert-success')
+    end
+
+    it "is possible to cancel adding a contact to a #{type.underscore}" do
+      visit model_path(@address)
+      click_on 'add_contact'
+      click_on :cancel
+      expect(current_path).to eq(model_path(@address))
+    end
+
+    describe "existing contacts" do
+      before(:each){ @contact = create :contact, organization: @address }
+
+      it "is possible to edit a contact of a #{type.underscore}" do
+        visit model_path(@address)
+        click_on "edit_#{@contact.id}"
+        fill_in :contact_name, with: 'New Name'
+        click_on :submit
+        expect(current_path).to eq(model_path(@address))
+        expect(page).to have_selector('div.alert-success')
+      end
+
+      it "is possible to cancel editing a contact of a #{type.underscore}" do
+        visit model_path(@address)
+        click_on "edit_#{@contact.id}"
+        click_on :cancel
+        expect(current_path).to eq(model_path(@address))
+      end
+      
+      it "is possible to delete a contact of a #{type.underscore}" do
+        visit model_path(@address)
+        click_on "delete_#{@contact.id}"
+        expect(current_path).to eq(model_path(@address))
+        expect(page).to have_selector('div.alert-success')
+      end
+    end
+  end
+
+  def model_path(address)
+    send("#{address.type.underscore}_path", address)
   end
 end
 
