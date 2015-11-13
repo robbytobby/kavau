3c524b4e00586f2dd120e7082841d88981d058fc
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2015-11-13 20:09:26 +0100

    added restart task for cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      execute :touch, 'tmp/restart.txt'
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
