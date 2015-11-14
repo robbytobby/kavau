ad0b27712007ea3c7ae70997e4e718edd2fa66b5
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2015-11-14 12:56:00 +0100

    do not create unnecessary devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations, :confirmations]
 
   #resources :addresses, except: :index
   resources :balances, only: :index
