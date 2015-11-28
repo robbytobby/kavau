9814dc3ee79156d6999f8c43a3b1b917703b3cb9
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2015-11-28 18:49:53 +0100

    mailer config in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'git@github.com:robbytobby/kavau.git'
 # set :log_level, :debug
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings.yml', 'tmp/restart.txt')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -22,15 +22,24 @@ set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
+  after :deploy, :restart
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+      #invoke 'delayed_job:restart'
+    end
+  end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      execute :touch, 'tmp/restart.txt'
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
     end
   end
+
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   raise 'environment variable HOST not set !!' if ENV['HOST'].blank?
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.smtp_settings = Settings.smtp_settings
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
