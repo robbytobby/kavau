b06168bb9f61e07ab79b9447bcbed767e14675fd
Widu Wittekindt <widu@fahrradwerkstatt-freiburg.de>
2015-11-29 11:32:03 +0100

    refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'errors/not_found'
-
-  get 'errors/internal_server_error'
-
   concern(:has_contacts){ resources :contacts, except: [:index, :show] }
   concern(:has_accounts){ resources :accounts, except: [:index, :show] }
   concern(:has_credit_agreements){ resources :credit_agreements, except: [:index, :show] }
@@ -39,9 +35,9 @@ Rails.application.routes.draw do
     root to: 'project#show', as: :authenticated_root
   end
 
-  get "/404" => "errors#not_found"
-  get "/422" => "errors#change_rejected"
-  get "/500" => "errors#internal_server_error"
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#change_rejected"
+  get "/500", to: "errors#internal_server_error"
 
   root to: redirect('/users/sign_in')
   # The priority is based upon order of creation: first created -> highest priority.
