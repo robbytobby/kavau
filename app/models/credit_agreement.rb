45baac297c3173f1487b7333bd803d6d4b6d6f80
Widu Wittekindt <wittekindt@posteo.de>
2016-01-24 10:02:38 +0100

    dropped restrictions on credit_agreement amount + interest_rate

--- app/models/credit_agreement.rb
+++ app/models/credit_agreement.rb
@@ -15,9 +15,9 @@ class CreditAgreement < ActiveRecord::Base
   delegate :last_terminated_year, :year_terminated?, to: :creditor
 
   validates_presence_of :amount, :interest_rate, :cancellation_period, :account_id, :creditor_id
-  validates_numericality_of :amount, greater_than_or_equal_to: 500
+  validates_numericality_of :amount, greater_than_or_equal_to: 0
   validates_numericality_of :interest_rate, greater_than_or_equal_to: 0, less_than: 100
-  validates_numericality_of :cancellation_period, greater_than_or_equal_to: 3
+  validates_numericality_of :cancellation_period, greater_than_or_equal_to: 0
   validates_uniqueness_of :number, allow_blank: true
   validate :account_valid_for_credit_agreement?, :termination_date_after_payments
 
