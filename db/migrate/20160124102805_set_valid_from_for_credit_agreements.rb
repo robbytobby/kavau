dd802f2f78f51a8a0878cb496e19293d3b57361c
Widu Wittekindt <wittekindt@posteo.de>
2016-02-07 12:50:46 +0100

    bugfix in migration

--- db/migrate/20160124102805_set_valid_from_for_credit_agreements.rb
+++ db/migrate/20160124102805_set_valid_from_for_credit_agreements.rb
@@ -2,8 +2,8 @@ class SetValidFromForCreditAgreements < ActiveRecord::Migration
   def up
     CreditAgreement.paper_trail_off!
     CreditAgreement.all.each do |c|
-      c.valid_from ||= (c.payments.first.try(:date) || c.created_at.to_date)
-      c.save
+      valid_from ||= (c.payments.first.try(:date) || c.created_at.to_date)
+      c.update_column(:valid_from, valid_from)
     end
     CreditAgreement.paper_trail_on!
   end
